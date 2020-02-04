c549229a200c78bef42ee63de3c42711499acf5f
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2020-02-04 14:58:41 +0500

    init

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Программа, которая печатает строку "Hello World!"
+ *
+ * @author Nikita Bolshagin
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * Точка входа в программу
+     *
+     * @param args аргументы запуска программы
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
